e60b1f12f786b95100179c0638936154d4d1cf5c
anthonyA1214 <anthonyamiluddin1@gmail.com>
2025-08-27 09:30:19 +0800

    day 12

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -9,4 +9,8 @@ class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    public function job() {
+        return $this->hasMany(Job::class);
+    } 
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -13,4 +13,12 @@ class Job extends Model
     protected $table = 'jobs_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class, 'job_tag', 'job_listing_id', 'tag_id');
+    }
 }
